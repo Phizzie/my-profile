0f25671294c74626cd40d3ed7c8c516e3ba287d2
Olupoju <olupojua@gmail.com>
2023-06-26 07:07:52 -0700

    my changes

---
+++ assets/script/scrpt.js
@@ -0,0 +1,19 @@
+// let name = document.querySelectorAll (".name");
+// let mail = document.querySelectorAll (".mail");
+// let message = document.querySelectorAll (".message");
+// let input = document.querySelectorAll ("input")
+// input.addEventListener("input",()=>{
+//     if(".name" >=1){
+//         console.log("is true")
+//     }
+// })
+
+let call = document.querySelector(".call")
+// let button = document.querySelectorAll("button")!
+    if(call > 0){
+        console.log("welcome")
+    }else(call < 0)
+    {
+        console.log("not welcome")
+    }
+
